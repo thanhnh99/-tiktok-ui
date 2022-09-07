7bdd1803d2e726a3016f2d768e6959fdde6c53c6
thanh.nguyenhuu <huuthanh99hn@gmail.com>
2022-09-08 03:57:25 +0700

    add base

---
+++ src/routes/index.js
@@ -0,0 +1,16 @@
+import Home from '~/pages/Home'
+import Following from '~/pages/Following'
+import Upload from '~/pages/Upload'
+
+//public Routes
+export const publicRoutes = [
+    {path: '/', component : Home},
+    {path: '/following', component : Following},
+    {path: '/upload', component : Upload, layout: null},
+]
+
+
+//login require
+export const privateRoutes = [
+    
+]
