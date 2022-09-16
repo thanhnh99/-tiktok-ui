9a053150b675d0372d4d6789be5a8eac60aec323
thanh.nguyenhuu <huuthanh99hn@gmail.com>
2022-09-17 02:22:36 +0700

    header #5: sub menu

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -24,6 +24,21 @@ const MENU_ITEMS = [
     {
         icon: <FontAwesomeIcon icon={faEarthAmerica} />,
         title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'English',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+            ],
+        },
     },
     {
         icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -44,6 +59,15 @@ function Header() {
             setSearchResult([]);
         }, 1000);
     }, []);
+
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                //handle change language
+                break;
+            default:
+        }
+    };
 
     return (
         <header className={cx('wrapper')}>
@@ -87,9 +111,7 @@ function Header() {
                     <Button text>Upload</Button>
                     <Button primary>Login</Button>
 
-                    <Menu
-                        items = {MENU_ITEMS}
-                    >
+                    <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                         <button className={cx('more-btn')}>
                             <FontAwesomeIcon icon={faEllipsisVertical} />
                         </button>
--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -5,12 +5,28 @@ import Tippy from '@tippyjs/react/headless';
 
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import MenuItem from './MenuItem';
+import Header from './Header';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
-function Menu({ children, items = [] }) {
+
+const defaultFn = (state) => {}
+function Menu({ children, items = [], onChange = defaultFn }) {
+
+    const [history, setHistory] = useState([{data: items}])
+    const current = history[history.length - 1];
+
     const renderItems = () => {
-        return items.map((item, index) => 
-            <MenuItem key={index} data={item} />
+        return current.data.map((item, index) => {
+            const isParent = !!item.children
+            return <MenuItem key={index} data={item} onClick = {() => {
+                if(isParent) {
+                    setHistory(prev => [...prev, item.children])
+                } else {
+                    onChange(item)
+                }
+            }}/>
+        }
         );
     };
 
@@ -21,7 +37,12 @@ function Menu({ children, items = [] }) {
             interactive={true}
             render={(attrs) => (
                 <div className={cx('menu-item-list')} tabIndex="-1" {...attrs}>
-                    <PopperWrapper className={cx('menu-popper')}>{renderItems()}</PopperWrapper>
+                    <PopperWrapper className={cx('menu-popper')}>
+                        {history.length > 1 && <Header title = {current.title} onBack = {() => {
+                            setHistory(prev => prev.slice(0, prev.length -1))
+                        }}/>}
+                        {renderItems()}
+                    </PopperWrapper>
                 </div>
             )}
         >
